738f3ab74b2c8550ddbbce37a6396d7ccc636865
MSymbios <50481178+s2erge@users.noreply.github.com>
2023-01-23 16:05:48 +0000

    ADD: Implement Fabric Development Environment for Minecraft 1.19.3"

---
+++ rlovelyr_fabric-1.19.3/src/main/java/net/msymbios/rlovelyr/LovelyRobotMod.java
@@ -0,0 +1,19 @@
+package net.msymbios.rlovelyr;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LovelyRobotMod implements ModInitializer {
+
+	// -- Variables --
+	public static final String MODID = "rlovelyr";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	// -- Methods --
+	@Override
+	public void onInitialize() {
+
+	} // onInitialize ()
+
+} // Class LovelyRobotMod
---
+++ rlovelyr_fabric-1.19.3/src/main/java/net/msymbios/rlovelyr/LovelyRobotModClient.java
@@ -0,0 +1,13 @@
+package net.msymbios.rlovelyr;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LovelyRobotModClient implements ClientModInitializer {
+
+    // -- Method --
+    @Override
+    public void onInitializeClient() {
+
+    } // onInitializeClient ()
+
+} // Class LovelyRobotClient
