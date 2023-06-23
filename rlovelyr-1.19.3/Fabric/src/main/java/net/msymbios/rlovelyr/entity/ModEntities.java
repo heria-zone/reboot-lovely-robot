5081b8ee7d15bee80002af4c0dd99874674a1577
MSymbios <50481178+s2erge@users.noreply.github.com>
2023-06-23 12:42:27 +0100

    REF: Increased collider height

--- rlovelyr-1.19.3/Fabric/src/main/java/net/msymbios/rlovelyr/entity/ModEntities.java
+++ rlovelyr-1.19.3/Fabric/src/main/java/net/msymbios/rlovelyr/entity/ModEntities.java
@@ -23,13 +23,13 @@ public class ModEntities {
 
     // -- Variables --
     public static final EntityType<BunnyEntity> BUNNY = Registry.register(Registries.ENTITY_TYPE, new Identifier(LovelyRobotMod.MODID, "bunny"),
-            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, BunnyEntity::new).dimensions(EntityDimensions.fixed(0.4f, 1.5f)).build());
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, BunnyEntity::new).dimensions(EntityDimensions.fixed(0.4f, 2F)).build());
     public static final EntityType<Bunny2Entity> BUNNY2 = Registry.register(Registries.ENTITY_TYPE, new Identifier(LovelyRobotMod.MODID, "bunny2"),
-            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, Bunny2Entity::new).dimensions(EntityDimensions.fixed(0.4f, 1.5f)).build());
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, Bunny2Entity::new).dimensions(EntityDimensions.fixed(0.4f, 2F)).build());
     public static final EntityType<HoneyEntity> HONEY = Registry.register(Registries.ENTITY_TYPE, new Identifier(LovelyRobotMod.MODID, "honey"),
-            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, HoneyEntity::new).dimensions(EntityDimensions.fixed(0.4f, 1.5f)).build());
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, HoneyEntity::new).dimensions(EntityDimensions.fixed(0.4f, 2F)).build());
     public static final EntityType<VanillaEntity> VANILLA = Registry.register(Registries.ENTITY_TYPE, new Identifier(LovelyRobotMod.MODID, "vanilla"),
-            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, VanillaEntity::new).dimensions(EntityDimensions.fixed(0.4f, 1.5f)).build());
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, VanillaEntity::new).dimensions(EntityDimensions.fixed(0.4f, 2F)).build());
 
     // -- Methods --
     public static void registerEntityAttribute(){
