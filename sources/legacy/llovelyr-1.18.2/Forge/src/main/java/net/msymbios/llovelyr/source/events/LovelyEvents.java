ea2c7bc8e2eb70fadfbe26f3990918be752c94c2
MSymbios <50481178+s2erge@users.noreply.github.com>
2025-11-03 08:37:41 +0000

    REF: Update 1.18.2 LovelyEvents implementation
    - Refine LovelyEvents implementation for 1.18.2 compatibility
    - Optimize event handling for older Minecraft version

--- sources/legacy/llovelyr-1.18.2/Forge/src/main/java/net/msymbios/llovelyr/source/events/LovelyEvents.java
+++ sources/legacy/llovelyr-1.18.2/Forge/src/main/java/net/msymbios/llovelyr/source/events/LovelyEvents.java
@@ -1,2 +1,60 @@
-package net.msymbios.llovelyr.source.events;public class LovelyEvents {
-}
+package net.msymbios.llovelyr.source.events;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.msymbios.llovelyr.LovelyLegacy;
+
+public class LovelyEvents {
+
+    // -- Nested Classes --
+
+    @Mod.EventBusSubscriber(modid = LovelyLegacy.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.DEDICATED_SERVER)
+    public static class ServerEvents {
+
+        // -- Custom Methods --
+
+        @SubscribeEvent
+        public void onServerStarting(ServerStartingEvent event) {
+            // Do something when the server starts
+            LovelyLegacy.LOGGER.info("HELLO FROM SERVER: STARTING");
+        } // onServerStarting ()
+
+    } // Class: ServerEvents
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = LovelyLegacy.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientEvents {
+
+        // -- Custom Methods --
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LovelyLegacy.LOGGER.info("HELLO FROM CLIENT: SETUP");
+            LovelyLegacy.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        } // onClientSetup ()
+
+    } // Class: ClientEvents
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+
+        // -- Custom Methods --
+
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // Register a new block here
+            LovelyLegacy.LOGGER.info("HELLO from Register Block");
+        } // onBlocksRegistry ()
+
+    } // Class: RegistryEvents
+
+} // Class: LovelyEvents
